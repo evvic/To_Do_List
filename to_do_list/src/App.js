e2d6d7b96365bb4bed8ca3d6fc893eceb023441a
Eric Brown <ewic98@gmail.com>
2021-10-17 00:16:03 +0300

    three seperate views

--- to_do_list/src/App.js
+++ to_do_list/src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {BrowserRouter, Route, Switch, Link} from 'react-router-dom';
+import {connect} from 'react-redux';
+import Info from './info';
+import Completed from './completed';
+
+class App extends React.Component {
+  render() {
+    const navClass = 'colors' + this.props.colorModeFromReduxStore;
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <nav className={navClass}>
+            <ul>
+              <li className="nav-item">
+                <Link to="/">Tasks</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/completed">Completed</Link>
+              </li>
+              <li className="last-nav-item">
+                <Link to="/info">Info</Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route path="/" exact>
+              <div><h1>Main Page</h1> Lorem ipsum etc.</div>
+            </Route>
+            <Route path="/completed" component={Completed} />
+            <Route path="/info" component={Info} />
+            <Route path="/">
+              <h1>Do not do that!</h1>
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
-export default App;
+// This function is needed to get data from Redux store 
+// to the component as props using connect() of react-redux. 
+// Reshape the data from store to a suitable form for the 
+// component here.
+const mapStateToProps = state => {
+  return {
+    colorModeFromReduxStore: state['general']['colorMode']
+  };
+};
+const app = connect(mapStateToProps, null)(App)
+
+export default app;
---
+++ to_do_list/src/info.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+function Info() {
+
+    return(
+        <div className="info">
+            <h1>Info Page</h1> Fill this with info information.
+        </div>
+    )
+}
+
+export default Info;
