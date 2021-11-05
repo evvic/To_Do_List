d93fc4b9cf27589fda1829ea6c70705c2bf562c6
Eric Brown <ewic98@gmail.com>
2021-11-06 00:44:02 +0200

    order by last modified for completed page

--- to_do_list/src/pages/completedpage.js
+++ to_do_list/src/pages/completedpage.js
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from 'react'
 import Filter from '../components/filter'
 import Tasks from '../tasks/tasks'
 import Search from '../components/search'
+import OrderBy from '../components/orderby'
 
 function CompletedPage() {
     console.log("in Completed Page")
     const [allTags, setAllTags] = useState([])
     const [filterTag, setFilterTag] = useState(null)
     const [searchText, setSearchText] = useState(null)
+    const [orderBy, setOrderBy] = useState("order")
 
     useEffect(() => {
         console.log("all tags back in task page", allTags)
@@ -22,7 +24,10 @@ function CompletedPage() {
             {/* change so isntead of using Completed component is uses Tasks component dynamically */}
             <Filter allTags={allTags} setFilterTag={setFilterTag} filterTag={filterTag} />
             <h1>Completed Tasks</h1>
-            <Search searchText={searchText} setSearchText={setSearchText} />
+            <div className="wrap-filters">
+                <Search searchText={searchText} setSearchText={setSearchText} />
+                <OrderBy orderBy={orderBy} setOrderBy={setOrderBy}/>
+            </div>
             <Tasks completed={"completed"} setAllTags={setAllTags} allTags={allTags} filterTag={filterTag}
                 searchText={searchText}/>
         </div>
